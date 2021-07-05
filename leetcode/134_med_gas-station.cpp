b01aee5c81507c7fb11277ba7cab2b4b70fd09ee
saksham36 <sakshamconsul@gmail.com>
2021-07-05 09:14:46 +0530

    Q134 Gas Stations Alt solution

--- leetcode/134_med_gas-station.cpp
+++ leetcode/134_med_gas-station.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        queue<int> dq;
-        int curr = 0, i = 0;
-        while(dq.size() != gas.size()){
-            dq.push(i);
+        int curr =0, prev = 0, start = 0;
+        for(int i=0; i<gas.size();i++){
             curr += gas[i] - cost[i];
-            i = (i+1) % gas.size();
-            while(!dq.empty() && curr <0){
-                curr -= (gas[dq.front()] - cost[dq.front()]);
-                if(dq.front() == gas.size()-1)
-                    return -1;
-                dq.pop();
+            if(curr < 0){
+                prev += curr;
+                curr = 0;
+                start = i+1;
             }
-            
         }
-        return dq.front();
+        return (prev+curr >=0)?start: -1;
     }
 };
