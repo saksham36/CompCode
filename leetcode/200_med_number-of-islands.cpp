4223b593dbe9199e1e82d50033f7102f3d5970ba
Saksham Consul <sakshamconsul@gmail.com>
2023-03-29 01:05:55 -0700

    More readable format of q200

--- leetcode/200_med_number-of-islands.cpp
+++ leetcode/200_med_number-of-islands.cpp
@@ -1,20 +1,41 @@
-class Solution {
-public:
-    int dfs(vector<vector<char>>& grid, int i, int j){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0')
-            return 0;
-        
-        grid[i][j]='0';
-        return 1+dfs(grid,i+1,j)+dfs(grid,i-1,j)+dfs(grid,i,j+1)+dfs(grid,i,j-1) ;
-        
+class Solution
+{
+private:
+    void dfs(vector<vector<char>> &grid, int r, int c)
+    {
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        grid[r][c] = '0';
+        if (r - 1 >= 0 && grid[r - 1][c] == '1')
+            dfs(grid, r - 1, c);
+        if (r + 1 < rows && grid[r + 1][c] == '1')
+            dfs(grid, r + 1, c);
+        if (c - 1 >= 0 && grid[r][c - 1] == '1')
+            dfs(grid, r, c - 1);
+        if (c + 1 < cols && grid[r][c + 1] == '1')
+            dfs(grid, r, c + 1);
     }
-    int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
-        for(int i=0; i<grid.size();i++){
-            for(int j =0; j<grid[0].size();j++){
-                if(dfs(grid, i,j)) count++;
+
+public:
+    int numIslands(vector<vector<char>> &grid)
+    {
+        if (grid.size() == 0)
+            return 0;
+
+        int num_islands = 0;
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    cout << "i: " << i << " j: " << j << endl;
+                    num_islands++;
+                    dfs(grid, i, j);
+                }
             }
         }
-        return count;
+        return num_islands;
     }
 };
