1e36add8c8e168a8af4dc4a01dcff4ea2f06977b
Saksham Consul <sakshamconsul@gmail.com>
2023-04-27 11:46:43 -0700

    Q48

--- leetcode/48_med_rotate-image.cpp
+++ leetcode/48_med_rotate-image.cpp
@@ -3,18 +3,17 @@ class Solution
 public:
     void rotate(vector<vector<int>> &matrix)
     {
-        uint n = matrix[0].size();
-        int tmp;
-        for (uint i = 0; i < n / 2 + n % 2; i++)
+        int n = matrix[0].size();
+        for (int i = 0; i < n; i++)
         {
-            for (uint j = 0; j < n / 2; j++)
+            for (int j = 0; j < i; j++)
             {
-                tmp = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
-                matrix[j][n - 1 - i] = matrix[i][j];
-                matrix[i][j] = tmp;
+                swap(matrix[i][j], matrix[j][i]);
             }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
