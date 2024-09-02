8bbd75df2db3d8ca4b94758dbb0a525c39450e5a
Saksham Consul <sakshamconsul@gmail.com>
2024-09-02 15:59:25 -0700

    Adding questions from Databricks leetcode

--- leetcode/48_med_rotate-image.cpp
+++ leetcode/48_med_rotate-image.cpp
@@ -3,17 +3,18 @@ class Solution
 public:
     void rotate(vector<vector<int>> &matrix)
     {
-        int n = matrix[0].size();
-        for (int i = 0; i < n; i++)
+        uint n = matrix[0].size();
+        int tmp;
+        for (uint i = 0; i < n / 2 + n % 2; i++)
         {
-            for (int j = 0; j < i; j++)
+            for (uint j = 0; j < n / 2; j++)
             {
-                swap(matrix[i][j], matrix[j][i]);
+                tmp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = tmp;
             }
-        }
-        for (int i = 0; i < n; i++)
-        {
-            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
